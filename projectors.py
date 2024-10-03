89e1b4d08401311599087fdb0854e9112909906e
macintol <macintol@oregonstate.edu>
2024-10-03 10:36:33 -0700

    final testing of projector decomposition

--- projectors.py
+++ projectors.py
@@ -32,11 +32,15 @@ def add_single_proj_gate_conj(phi, theta, cr, qubits):
 
 def add_beamsplitter(top_index, angle, cr):
     """Helper function to insert beamsplitter for a given angle (representing the reflectivity)"""
+    cr.x([top_index, top_index+1])
     cr.cz(top_index, top_index+1) 
+    cr.x([top_index, top_index+1])
     cr.cx(top_index, top_index+1)
     cr.cry(angle, top_index+1, top_index)
     cr.cx(top_index, top_index+1)
+    cr.x([top_index, top_index+1])
     cr.cz(top_index, top_index+1)
+    cr.x([top_index, top_index+1])
 
 def add_mult_proj_bot(top_index, ancilla, cr):
     """Helper function to insert projector for bottom arm of interferomter (both B and Bconj)"""
